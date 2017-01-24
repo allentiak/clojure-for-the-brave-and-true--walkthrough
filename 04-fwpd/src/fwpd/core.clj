22832ff0a96e83084eb38cdad2acf1ae0c9358db
Leandro Doctors <ldoctors@gmail.com>
2017-01-23 21:05:59 -0300

    Add titles for exercises 4.x

--- 04-fwpd/src/fwpd/core.clj
+++ 04-fwpd/src/fwpd/core.clj
@@ -31,20 +31,24 @@
                  (map vector vamp-keys unmapped-row)))
        rows))
 
+;; Exercise 4.1
 (defn glitter-filter
   "Filter a seq of record maps, returning those with glitter-index higher than minimum-glitter"
   [minimum-glitter records]
   (map :name (filter #(>= (:glitter-index %) minimum-glitter) records)))
 
+;; Exercise 4.2
 (defn append
   [suspects-list {:keys [name glitter-index] :as new-suspect}]
   (conj suspects-list new-suspect))
 
+;; Exercise 4.3
 (defn validate?
   "Verify whether a record contains all its keywords"
   [keywords-list record]
   (every? #(true? %) (vec (map #(false? (nil? (% record))) keywords-list))))
 
+;; Exercise 4.4
 (defn unmapify
   "Convert a seq of record maps into a seq of rows of columns"
   [maps]
