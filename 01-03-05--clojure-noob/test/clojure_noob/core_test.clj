c518fdc51970306120e7e846231fe43a8675d5f7
Leandro Doctors <ldoctors@gmail.com>
2019-01-14 18:46:17 -0300

    Add tests for Excercises 3.2, 3.3 and 3.4

--- 01-03-05--clojure-noob/test/clojure_noob/core_test.clj
+++ 01-03-05--clojure-noob/test/clojure_noob/core_test.clj
@@ -2,6 +2,19 @@
   (:require [clojure.test :refer :all]
             [clojure-noob.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+;;; Chapter 3
+
+;; Exercise 3.2
+(deftest add100-test
+  (is (= (add100 1) 101))
+  (is (= (add100 -100) 0)))
+
+;; Exercise 3.3
+(deftest dec-maker-test
+  (is (= ((dec-maker 100) 101) 1))
+  (is (= ((dec-maker 100) 0) -100)))
+
+;; Exercise 3.4
+(deftest mapset-test
+  (is (= (mapset inc [1 1 2 2]) #{2 3})))
